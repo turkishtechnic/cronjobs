3c9f281245c0db845ed21fb1158b0ef73068deb1
Abdussamet Kocak <abdus@abdus.dev>
2021-03-12 12:50:35 +0300

    Refactor names

--- src/TT.Cronjobs/CronjobsOptions.cs
+++ src/TT.Cronjobs/CronjobsOptions.cs
@@ -3,10 +3,10 @@
     public class CronjobsOptions
     {
         public const string Key = "Cronjobs";
-        public string RegistrationApiUrl { get; set; }
+        public string ApiBaseUrl { get; set; }
         public string UrlTemplate { get; set; }
         public int RetryCount { get; set; } = 5;
-        public int WaitSeconds { get; set; } = 5;
+        public int WaitSeconds { get; set; } = 10;
         public int TimeoutSeconds { get; set; } = 60;
     }
 }
