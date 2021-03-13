f1ea4671d2e8de4fa4da6d965554650800261e4d
Abdussamet Kocak <abdus@abdus.dev>
2021-03-13 16:09:53 +0300

    [refactor] Refactor cronjob registration into a separate class

--- src/TT.Cronjobs.Blitz/CronjobBroadcasterService.cs
+++ src/TT.Cronjobs.Blitz/CronjobBroadcasterService.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
@@ -13,7 +11,6 @@ namespace TT.Cronjobs.Blitz
     internal class CronjobBroadcasterService : BackgroundService
     {
         private readonly BlitzOptions _options;
-        private readonly CronjobWebhookProvider _cronjobWebhookProvider;
         private readonly ICronjobBroadcaster _broadcaster;
         private readonly ILogger<CronjobBroadcasterService> _logger;
 
@@ -26,29 +23,15 @@ namespace TT.Cronjobs.Blitz
             _options = options.Value;
             _broadcaster = broadcaster;
             _logger = logger;
-            _cronjobWebhookProvider = cronjobWebhookProvider;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             await Task.Delay(TimeSpan.FromSeconds(_options.WaitSeconds), stoppingToken);
-
-            _logger.LogInformation("Finding cronjobs");
-            List<CronjobWebhook> jobs;
+            
             try
             {
-                jobs = _cronjobWebhookProvider.Cronjobs.ToList();
-                _logger.LogInformation($"Found {jobs.Count} cronjobs. Broadcasting all cronjobs");
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, "An error occurred while discovering cronjobs");
-                throw;
-            }
-
-            try
-            {
-                await _broadcaster.BroadcastAsync(jobs, stoppingToken);
+                await _broadcaster.BroadcastAsync(stoppingToken);
                 _logger.LogInformation("Jobs have been broadcast successfully");
             }
             catch (Exception e)
--- src/TT.Cronjobs.Blitz/ICronjobBroadcaster.cs
+++ src/TT.Cronjobs.Blitz/ICronjobBroadcaster.cs
@@ -1,12 +1,62 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using TT.Cronjobs.AspNetCore;
 
 namespace TT.Cronjobs.Blitz
 {
     internal interface ICronjobBroadcaster
     {
-        Task BroadcastAsync(IEnumerable<CronjobWebhook> jobs, CancellationToken cancellationToken);
+        Task BroadcastAsync(CancellationToken cancellationToken = default);
+    }
+
+    internal class CronjobRegistrationBroadcaster : ICronjobBroadcaster
+    {
+        private readonly ICronjobApiClient _cronjobApi;
+        private readonly IHostEnvironment _environment;
+        private readonly IVersionProvider _versionProvider;
+        private readonly ILogger<CronjobRegistrationBroadcaster> _logger;
+        private readonly CronjobWebhookProvider _cronjobWebhookProvider;
+
+        public CronjobRegistrationBroadcaster(IHostEnvironment environment,
+                                              ICronjobApiClient cronjobApi,
+                                              IVersionProvider versionProvider,
+                                              ILogger<CronjobRegistrationBroadcaster> logger,
+                                              CronjobWebhookProvider cronjobWebhookProvider)
+        {
+            _environment = environment;
+            _cronjobApi = cronjobApi;
+            _versionProvider = versionProvider;
+            _logger = logger;
+            _cronjobWebhookProvider = cronjobWebhookProvider;
+        }
+
+        public async Task BroadcastAsync(CancellationToken cancellationToken = default)
+        {
+            _logger.LogInformation("Finding cronjobs");
+            List<CronjobWebhook> jobs;
+            try
+            {
+                jobs = _cronjobWebhookProvider.Cronjobs.ToList();
+                _logger.LogInformation($"Found {jobs.Count} cronjobs");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "An error occurred while discovering cronjobs");
+                throw;
+            }
+
+            var payload = new ProjectBatchRegistration
+            {
+                Title = $"{_environment.ApplicationName} ({_environment.EnvironmentName})",
+                Version = _versionProvider.Version,
+                Cronjobs = jobs
+            };
+            await _cronjobApi.BatchRegisterProjectAsync(payload, cancellationToken);
+        }
     }
 }
