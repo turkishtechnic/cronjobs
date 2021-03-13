91efbe618af56babf4ab8c95fb56df952feb3216
Abdussamet Kocak <abdus@abdus.dev>
2021-03-13 16:18:45 +0300

    [fix] Reduce retries

--- src/TT.Cronjobs.Blitz/BlitzOptions.cs
+++ src/TT.Cronjobs.Blitz/BlitzOptions.cs
@@ -3,7 +3,7 @@
     public class BlitzOptions
     {
         public string ApiBaseUrl { get; set; }
-        public int RetryCount { get; set; } = 5;
+        public int RetryCount { get; set; } = 3;
         public int WaitSeconds { get; set; } = 10;
         public int TimeoutSeconds { get; set; } = 60;
     }
