8d00f38dba67f6cbb037457a0016db2f11e17f9e
Abdussamet Kocak <abdus@abdus.dev>
2021-03-13 15:12:57 +0300

    [test] Add tests for Blitz integration

---
+++ src/TT.Cronjobs.Tests/BlitzTests.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TT.Cronjobs.AspNetCore;
+using TT.Cronjobs.Blitz;
+using Xunit;
+
+namespace TT.Cronjobs.Tests
+{
+    public class BlitzTests
+    {
+        [Fact]
+        public async Task CronjobsAreRegisteredAtStartup()
+        {
+            var blitzClient = new FakeBlitzClient();
+            var versionProvider = new FakeVersionProvider("testversion");
+            var host = await CreateHost(options =>
+                {
+                    options.WaitSeconds = 0;
+                    options.RetryCount = 0;
+                    options.ApiBaseUrl = "https://example.com";
+                },
+                apiClient: blitzClient,
+                versionProvider: versionProvider
+            );
+
+            // just in case
+            await Task.Delay(TimeSpan.FromSeconds(0.5));
+
+            Assert.NotNull(blitzClient.ProjectBatchRegistration);
+            Assert.NotEmpty(blitzClient.ProjectBatchRegistration.Cronjobs);
+            Assert.Equal(versionProvider.Version, blitzClient.ProjectBatchRegistration.Version);
+        }
+
+        private async Task<IHost> CreateHost(Action<BlitzOptions> configureBlitz,
+                                             ICronjobApiClient apiClient,
+                                             IVersionProvider versionProvider = null,
+                                             Assembly assembly = null)
+        {
+            var host = new HostBuilder()
+                .ConfigureWebHost(builder =>
+                {
+                    builder.UseTestServer()
+                        .ConfigureServices(services
+                            =>
+                        {
+                            services
+                                .AddCronjobs(options =>
+                                    {
+                                        options.RoutePattern = "cronjobs";
+                                        options.WebhookBaseUrl = "https://example.com";
+                                    },
+                                    assembly ?? Assembly.GetExecutingAssembly()
+                                )
+                                .UseBlitz(configureBlitz);
+                            services.AddSingleton<ICronjobApiClient>(apiClient);
+                            services.AddTransient<ICronjobProvider, FakeCronjobProvider>();
+                            services.AddTransient<IVersionProvider>(provider => versionProvider ?? new StubVersionProvider());
+                            services.AddTransient<SimpleCronjob>();
+
+                            services.AddRouting();
+                        })
+                        .Configure(app
+                            => app.UseRouting().UseEndpoints(routeBuilder => { routeBuilder.MapCronjobWebhook(); }));
+                }).Build();
+            await host.StartAsync();
+            return host;
+        }
+
+        class FakeCronjobProvider : ICronjobProvider
+        {
+            public IEnumerable<CronjobInfo> Cronjobs { get; } = new[]
+            {
+                new CronjobInfo(typeof(SimpleCronjob))
+            };
+        }
+
+        class FakeBlitzClient : ICronjobApiClient
+        {
+            public ProjectBatchRegistration ProjectBatchRegistration { get; set; }
+            public Dictionary<string, StatusUpdate> StatusUpdates { get; set; } = new Dictionary<string, StatusUpdate>();
+
+            public Task BatchRegisterProjectAsync(ProjectBatchRegistration registration, CancellationToken cancellationToken = default)
+            {
+                ProjectBatchRegistration = registration;
+                return Task.CompletedTask;
+            }
+
+            public Task UpdateExecutionStatusAsync(string executionId, StatusUpdate update, CancellationToken cancellationToken = default)
+            {
+                StatusUpdates.Add(executionId, update);
+                return Task.CompletedTask;
+            }
+        }
+
+        class FakeVersionProvider : IVersionProvider
+        {
+            private readonly string _version;
+            public FakeVersionProvider(string version) => _version = version;
+            public string Version => _version;
+        }
+
+        class StubVersionProvider : IVersionProvider
+        {
+            public string Version => "version";
+        }
+    }
+}
